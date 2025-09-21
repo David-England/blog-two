bfbc6667d1092a6d0a9244e4a92912f9aeb3f3e4
David England <david.england.de@googlemail.com>
2025-09-21 18:08:06 +0100

    Add BlogLinkTile component; create BlogMetadata type
    To do: change the tile's date format to use d MMMM yyyy.

---
+++ src/types/blog-metadata.ts
@@ -0,0 +1,8 @@
+export default interface BlogMetadata {
+    title: string,
+    authorForename: string,
+    authorSurname: string,
+    topic: string,
+    datePublished: Date,
+    url: string
+}
